c70bd2e13f7a7a1ced55ffd156be460dfd2772e5
4lch4 <git@4lch4.email>
2024-06-19 17:59:03 -0500

    feat: added functionality for project
    This commit contains the code necessary to consider this project complete and a success.

---
+++ main.py
@@ -0,0 +1,41 @@
+def get_letters_count(text):
+    letters = {}
+
+    for letter in text:
+        if letter.isalpha():
+            letter = letter.lower()
+
+            if letter in letters:
+                letters[letter] += 1
+            else:
+                letters[letter] = 1
+
+    return letters
+
+
+def get_word_count(text):
+    return len(text.split())
+
+
+def main():
+    print("--- Frankenstein Report ---\n")
+
+    with open("./books/frankenstein.txt", "r") as file:
+        text = file.read()
+
+        word_count = get_word_count(text)
+        letters_count = get_letters_count(text)
+
+        print(f"{word_count} words found in the text.\n")
+        
+        for letter, count in letters_count.items():
+            print(f"The letter '{letter}' appears {count} times in the text.")
+            # print(f"{letter}: {count}")
+
+        # print(f"Word count: {word_count}")
+        # print(f"Letters count: {letters_count}")
+
+        # print(text[:25])
+
+
+main()
